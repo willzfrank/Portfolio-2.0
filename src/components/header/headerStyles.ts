2e5e08962cd0fee75a5b49f9b55f6c7004bd2d62
Godswill Francis <willzudoh4@gmail.com>
2023-03-03 18:38:57 -0800

    fixed the font for header

--- src/components/header/headerStyles.ts
+++ src/components/header/headerStyles.ts
@@ -11,19 +11,19 @@ export const HeaderTitle = styled.h1`
   transition: all 0.2s ease-in-out;
 
   @media only screen and (max-width: 884px) and (min-width: 430px) {
-    font-size: 9.5vh;
+    font-size: 9.5vh !important;
     line-height: 1.3;
     padding: 2rem 0.5rem;
   }
 
   @media only screen and (max-width: 429px) {
-    font-size: 3.9em;
+    font-size: 8.5vh !important;
     line-height: 1.3;
     padding: 1rem 0.2rem;
   }
 
   @media only screen and (min-width: 320px) and (max-width: 392px) {
-    font-size: 3.4em;
+    font-size: 9.03vh !important;
     line-height: 1.3;
     padding: 1.5rem 0.5rem;
   }
