2311a9915dbca4fed1ba16aa9938c1289c68e38a
Godswill Francis <willzudoh4@gmail.com>
2023-04-19 10:45:22 -0700

    fixeed pointer bug

--- src/components/pointer/pointer.styles.ts
+++ src/components/pointer/pointer.styles.ts
@@ -1,7 +1,21 @@
+// import styled from 'styled-components';
+
+// export const PointerContainer = styled.div`
+//   position: absolute;
+//   width: 3rem;
+//   height: 3rem;
+//   border-radius: 50%;
+//   pointer-events: none;
+//   background-color: transparent;
+//   box-shadow: 0px 0px 10px 0px #dbdada;
+//   transform: translate(-50%, -50%);
+//   transition: 0.3s ease-in-out;
+// `;
+
 import styled from 'styled-components';
 
 export const PointerContainer = styled.div`
-  position: absolute;
+  position: fixed;
   width: 3rem;
   height: 3rem;
   border-radius: 50%;
@@ -10,4 +24,5 @@ export const PointerContainer = styled.div`
   box-shadow: 0px 0px 10px 0px #dbdada;
   transform: translate(-50%, -50%);
   transition: 0.3s ease-in-out;
+  z-index: 9999;
 `;
