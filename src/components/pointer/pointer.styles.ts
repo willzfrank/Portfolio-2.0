fb4dfe4acd31fa957317fbc596de1cdb802eaba6
Godswill Francis <willzudoh4@gmail.com>
2023-04-19 10:49:16 -0700

    removed fixed pointer

--- src/components/pointer/pointer.styles.ts
+++ src/components/pointer/pointer.styles.ts
@@ -1,21 +1,7 @@
-// import styled from 'styled-components';
-
-// export const PointerContainer = styled.div`
-//   position: absolute;
-//   width: 3rem;
-//   height: 3rem;
-//   border-radius: 50%;
-//   pointer-events: none;
-//   background-color: transparent;
-//   box-shadow: 0px 0px 10px 0px #dbdada;
-//   transform: translate(-50%, -50%);
-//   transition: 0.3s ease-in-out;
-// `;
-
 import styled from 'styled-components';
 
 export const PointerContainer = styled.div`
-  position: fixed;
+  position: absolute;
   width: 3rem;
   height: 3rem;
   border-radius: 50%;
@@ -24,5 +10,4 @@ export const PointerContainer = styled.div`
   box-shadow: 0px 0px 10px 0px #dbdada;
   transform: translate(-50%, -50%);
   transition: 0.3s ease-in-out;
-  z-index: 9999;
 `;
